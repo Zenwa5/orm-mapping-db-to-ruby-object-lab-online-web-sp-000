71341dcc346e22617237839fb55e75e4f41df3b6
Ezenwa Onukwue <ezenwa.onukwue@salesloft.com>
2019-03-21 02:41:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -15,7 +15,7 @@ class Student
     SELECT * FROM students
     SQL
     
-    # remember each row should be a new instance of the Student class
+    DB[:conn].execute(sql)
   end
 
   def self.find_by_name(name)
