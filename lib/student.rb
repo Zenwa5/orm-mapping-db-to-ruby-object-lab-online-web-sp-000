0d43557b08957de6a2d82fc6314dc23493428cdc
Ezenwa Onukwue <ezenwa.onukwue@salesloft.com>
2019-03-21 02:45:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -20,6 +20,13 @@ class Student
     end
   end
 
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+    SELECT * FROM students
+    WHERE grade = 9
+    SQL
+  end
+
   def self.find_by_name(name)
     sql = <<-SQL
     SELECT * FROM students
